98d26cce1a1b72bf21b6701bd67e8d5a51dce89d
Donghee <handh0413@gmail.com>
2024-06-29 16:58:11 +0900

    fix random background images

--- src/js/background.js
+++ src/js/background.js
@@ -1,14 +1,13 @@
 const images = ['image01.jpg', 'image02.jpg', 'image03.jpg', 'image04.jpg'];
 
-let currentIndex = 0;
-
 function changeBackgroundImage() {
+  let currentIndex = Math.floor(Math.random() * images.length);
+  console.log('currentIndex', currentIndex);
   document.body.style.backgroundImage = `url(images/${images[currentIndex]})`;
-  currentIndex = (currentIndex + 1) % images.length;
 }
 
 // 첫 번째 이미지를 즉시 설정
 changeBackgroundImage();
 
 // 10초 간격으로 배경 이미지 변경
-setInterval(changeBackgroundImage, 10000);
+setInterval(changeBackgroundImage, 5000);
